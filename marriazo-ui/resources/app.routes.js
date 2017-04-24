e45ccc79ba37fe8e3bfd3eced0502523efb8b1c5
JatinBansal <JatinBansal@DESKTOP-7U5L6MD>
2017-04-25 00:18:41 +0530

    add otherwise clause

--- marriazo-ui/resources/app.routes.js
+++ marriazo-ui/resources/app.routes.js
@@ -1,16 +1,12 @@
 app.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "home.html"
-    })
-    .when("/search/banquet/:state/:city", {
-        templateUrl : "resources/views/search-results.tpl.html",
-        controller: "SearchController"
-    })
-    .when("/green", {
-        templateUrl : "green.htm"
-    })
-    .when("/blue", {
-        templateUrl : "blue.htm"
-    });
+	$routeProvider.when("/", {
+		templateUrl : "home.html"
+	}).when("/search/banquet/:state/:city", {
+		templateUrl : "resources/views/search-results.tpl.html",
+		controller : "SearchController"
+	}).when("/green", {
+		templateUrl : "green.htm"
+	}).when("/blue", {
+		templateUrl : "blue.htm"
+	}).otherwise("/");
 });
