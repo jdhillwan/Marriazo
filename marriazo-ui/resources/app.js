eb33a7d301651872d8c4770c24f747c432c6cde9
JatinBansal <JatinBansal@DESKTOP-7U5L6MD>
2017-04-26 23:27:08 +0530

    Add Dependencies

--- marriazo-ui/resources/app.js
+++ marriazo-ui/resources/app.js
@@ -1,10 +1,24 @@
-var app = angular.module('MarriazoApp', [ 'ui.bootstrap', 'ngRoute' ]);
+var app = angular.module('MarriazoApp', [ 'socialLogin','ui.bootstrap', 'ngRoute','toastr','ui-notification']);
 
-/*app.config(function(socialProvider) {
-	socialProvider.setGoogleKey("373292484064-qta6ts2ii99p53tam1giil7f1d4mqngk.apps.googleusercontent.com");
-	socialProvider.setLinkedInKey("YOUR LINKEDIN CLIENT ID");
-	socialProvider.setFbKey({
-		appId : "654355474752575",
-		apiVersion : "v2.8"
-	});
-});*/
+app
+		.config(function(socialProvider, $routeProvider) {
+			socialProvider
+					.setGoogleKey("373292484064-qta6ts2ii99p53tam1giil7f1d4mqngk.apps.googleusercontent.com");
+			/* socialProvider.setLinkedInKey("YOUR LINKEDIN CLIENT ID"); */
+			socialProvider.setFbKey({
+				appId : "654355474752575",
+				apiVersion : "v2.8"
+			});
+
+			$routeProvider.when("/", {
+				templateUrl : "home.html"
+			}).when("/search/banquet/:state/:city", {
+				templateUrl : "resources/views/search-results.tpl.html",
+				controller : "SearchController"
+			}).when("/green", {
+				templateUrl : "green.htm"
+			}).when("/blue", {
+				templateUrl : "blue.htm"
+			}).otherwise("/");
+
+		});
