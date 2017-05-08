b02761237b238960bc2c9f67c5a2dde3aaf8414d
jdhillwan <jdhillwan14@gmail.com>
2017-05-08 23:35:11 +0530

    Add files via upload

---
+++ UserDTO.java
@@ -0,0 +1,28 @@
+package com.webosoft.login;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import org.bson.BSONObject;
+
+import com.mongodb.BasicDBObject;
+
+public class UserDTO extends BasicDBObject implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserDTO() {
+		super();
+	}
+
+	public UserDTO(Map<?, ?> base) {
+		super(base);
+	}
+
+	public UserDTO(BSONObject base) {
+		super();
+		this.putAll(base);
+	}
+}
